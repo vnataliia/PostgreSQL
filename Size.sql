a4658ebb740bb23422584c83a3515df40e679862
vnataliia <nvelyka@gmail.com>
2019-10-30 15:13:45 +0200

    Update

--- Size.sql
+++ Size.sql
@@ -1,8 +1,12 @@
-SELECT table_schema, table_name, row_estimate,
+SELECT table_schema, table_name,
+       -- row_estimate,
+       CASE WHEN row_estimate/10^6 > 1 THEN (round((row_estimate/10^6)::numeric,2))::text || ' M' ELSE row_estimate::text END AS tuples,
        pg_size_pretty(total_bytes) AS total,
        pg_size_pretty(index_bytes) AS INDEX,
        pg_size_pretty(toast_bytes) AS toast,
-       pg_size_pretty(table_bytes) AS TABLE
+       pg_size_pretty(table_bytes) AS TABLE,
+       pg_size_pretty(sum(table_bytes + toast_bytes) OVER()) AS WO_ids,
+       pg_size_pretty(sum(total_bytes) OVER()) AS DB_size
   FROM (
          SELECT *, total_bytes-index_bytes-COALESCE(toast_bytes,0) AS table_bytes
            FROM (
@@ -17,3 +21,15 @@ SELECT table_schema, table_name, row_estimate,
                 ) a
        ) a
 ORDER BY total_bytes DESC;
+
+
+\l+
+\dt+
+SELECT pg_size_pretty(pg_database_size(current_database()));
+SELECT pg_size_pretty(sum(pg_database_size(datname))) from pg_database;
+
+select pg_size_pretty(pg_relation_size('table_name')); --only table
+select pg_size_pretty(pg_indexes_size('table_name')); --only indexes
+select pg_size_pretty(pg_total_relation_size('table_name')); -- table + index + toast
+
+                                             
